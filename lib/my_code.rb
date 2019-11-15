ed6408ae26a98bf5ce1557c9a1bbe279f11ade40
Alejandro Linan <jandrolinan@gmail.com>
2019-11-15 04:15:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,18 +13,24 @@ end
 #map(source_code){|n| n * -1}
 
 def reduce(source_array, starting_point = nil)
-  value = starting_point 
-  i = 0 
+  if starting_point
+    total = starting_point
+    i = 0 
+  else 
+    total = source_array[0]
+    i = 1 
+  end 
+   
   while i < source_array.length do 
     if yield(value, source_array[i])
-      value = (yield(value, source_array[i]))
-    elsif yield(value, source_array[i]) != true 
-      value = yield(value, source_array[i])
+      total = (yield(total, source_array[i]))
+    elsif yield(total, source_array[i]) != true 
+      total = yield(total, source_array[i])
     end 
     
     # value = yield(value, source_array[i])
     
     i += 1 
   end 
-  return value 
+  return total  
 end 

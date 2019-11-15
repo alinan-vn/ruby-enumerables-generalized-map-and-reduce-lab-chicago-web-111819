b7a0fc5a4a35fd454cd648260e0a22814bf842b3
Alejandro Linan <jandrolinan@gmail.com>
2019-11-15 03:51:13 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,12 +16,10 @@ def reduce(source_array, starting_point = 0)
   value = starting_point 
   i = 0 
   while i < source_array.length do 
-    #lines 19-22 was just something I was trying that would address the truthy, truth, and false parts of this lab 
-    # if yield(value, source_array[i])
-    #   value = yield(value, source_array[i])
-    # end 
+    if yield(value, source_array[i])
+      value = yield(value, source_array[i])
+    end 
     
-    # line 25 passes all the tests except for returning true when a truthy value is present, however I think the reason the other tests passed, was because of the order of the arrays of input
     # value = yield(value, source_array[i])
     
     i += 1 
